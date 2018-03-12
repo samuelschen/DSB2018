905e24ea0550d48fd84bc76139f488a8320ab1eb
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2018-03-12 15:40:10 +0800

    implement weighted focal loss function

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from torch.nn.functional import binary_cross_entropy
 
@@ -27,7 +28,7 @@ class SoftDiceLoss(nn.Module):
         return 1. - dice
 
 
-# Jaccard/IoU = Sum(PiGi) / (Sum(Pi) + Sum(Gi) - Sum(PiGi)) 
+# Jaccard/IoU = Sum(PiGi) / (Sum(Pi) + Sum(Gi) - Sum(PiGi))
 class IoULoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super().__init__()
@@ -43,12 +44,37 @@ class IoULoss(nn.Module):
         # three kinds of loss formulas: (1) 1 - iou (2) -iou (3) -torch.log(iou)
         return 1. - iou
 
+class FocalLoss(nn.Module):
+    """
+    Focal Loss for Dense Object Detection [https://arxiv.org/abs/1708.02002]
+    Digest the paper as below:
+        α, balances the importance of positive/negative examples
+        γ, focusing parameter that controls the strength of the modulating term
+
+            CE(pt) = −log(pt) ==> pt = exp(-CE)
+            FL(pt) = −α((1 − pt)^γ) * log(pt)
+
+        In general α should be decreased slightly as γ is increased (for γ = 2, α = 0.25 works best).
+    """
+    def __init__(self, focusing_param=2, balance_param=0.25):
+        super().__init__()
+        self.gamma = focusing_param
+        self.alpha = balance_param
+
+    def forward(self, inputs, targets, weights=None):
+        logpt = -binary_cross_entropy(inputs, targets, weights)
+        pt = torch.exp(logpt)
+        # compute the loss
+        focal_loss = -((1-pt)**self.gamma) * logpt
+        balanced_focal_loss = self.alpha * focal_loss
+        return balanced_focal_loss
+
 def criterion(preds, labels):
 # (1) BCE Loss
 #     return BinaryCrossEntropyLoss2d().forward(preds, labels)
 # (2) BCE Loss + DICE Loss
 #     return BinaryCrossEntropyLoss2d().forward(preds, labels) + \
-#            SoftDiceLoss().forward(preds, labels) 
+#            SoftDiceLoss().forward(preds, labels)
 # (3) BCE Loss + Jaccard/IoU Loss
     return BinaryCrossEntropyLoss2d().forward(preds, labels) + \
            IoULoss().forward(preds, labels)
@@ -62,4 +88,8 @@ def contour_criterion(preds, labels):
 
 def weight_criterion(preds, labels, weights):
     return binary_cross_entropy(preds, labels, weights) + \
+           IoULoss().forward(preds, labels)
+
+def focal_criterion(preds, labels, weights):
+    return FocalLoss().forward(preds, labels, weights) + \
            IoULoss().forward(preds, labels)
